89f8995b21f613ad71d832f689545ed2ab9b8244
bgallardov <brayan.gallardo@inetum.com>
2026-01-25 15:50:40 -0500

    Configuración Inicial

---
+++ src/main/java/com/project/bcngroup/BcngroupApplication.java
@@ -0,0 +1,13 @@
+package com.project.bcngroup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BcngroupApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BcngroupApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/bcngroup/BcngroupApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.bcngroup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BcngroupApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
